239fe6d7a476ce1a62ba819b3d0b83f0b74d2eba
HenriqueChepli <henriquechepli@gmail.com>
2023-11-06 13:45:05 -0300

    index

---
+++ js/global.js
@@ -0,0 +1,18 @@
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu() {
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+
+const btnBusca = document.getElementById('btn-busca');
+
+function toggleBusca() {
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('ativo');
+}
+
+btnBusca.addEventListener('click', toggleBusca)
